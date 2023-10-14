3a54c77f6369fc1f442235849d8c3ca557ac8574
tich.tereza <tich.tereza@gmail.com>
2023-10-14 14:08:46 +0200

    ukol-03

---
+++ ukol-03.py
@@ -0,0 +1,34 @@
+# Soubor body.json je JSON, který obsahuje informace o získaných bodech z písemky.
+
+# Soubor si ulož a načti do slovníku.
+
+# Z písemky nebude známka, ale jen Pass/Fail hodnocení neboli prospěl(a)/neprospěl(a). 
+# Vytvoř nový slovník. 
+# Jeho klíče budou opět jména žáků, a hodnotou bude řetězec "Pass", pokud má jednotlivec alespoň než 60 bodů. 
+# Pokud má méně než 60, hodnota bude "Fail".
+
+# Výsledný slovník ulož jako JSON do souboru prospech.json.
+
+
+import json
+
+with open("body.json", mode = "r", encoding="utf-8") as file: 
+    body = json.load(file) 
+
+# prazdny slovnik
+PassOrFail = {}
+
+for klic in body: 
+    # print(key)
+    vysledek = ""
+    skore = body[klic]
+    if skore >= 60:
+        vysledek = "Pass"
+    else:
+        vysledek = "Fail" 
+    PassOrFail[klic] = vysledek 
+
+print(PassOrFail)
+
+with open("prospech.json", mode="w", encoding="utf-8") as file:
+    json.dump(PassOrFail, file, indent=4)
