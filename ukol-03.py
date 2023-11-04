3e80b9eb342d314b28de9e1531828a1bc8390bd5
tich.tereza <tich.tereza@gmail.com>
2023-11-04 16:48:31 +0100

    update

--- ukol-03.py
+++ ukol-03.py
@@ -30,5 +30,20 @@ for klic in body:
 
 print(PassOrFail)
 
+# zlepšení od Liuby
+PassOrFail2 = {}
+
+for klic, skore in body.items():
+    if skore >= 60:
+        vysledek = "Pass"
+    else:
+        vysledek = "Fail" 
+    PassOrFail2[klic] = vysledek
+
+print(PassOrFail2)
+
 with open("prospech.json", mode="w", encoding="utf-8") as file:
-    json.dump(PassOrFail, file, indent=4)
+    json.dump(PassOrFail, file, ensure_ascii=False, indent=4)
+
+with open("prospech.json", mode="w", encoding="utf-8") as file:
+    json.dump(PassOrFail2, file, ensure_ascii=False, indent=4)
